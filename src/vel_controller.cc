a714966d992e20a0402218a3125f1d699a3328f8
Murphy <murphyqwq@gmail.com>
2022-12-24 13:56:22 +0800

    完成速度的坐标系变换

--- src/vel_controller.cc
+++ src/vel_controller.cc
@@ -10,36 +10,36 @@
 #include <gazebo/transport/transport.hh>
 
 /////////////////////////////////////////////////
-int main(int _argc, char **_argv) {
-	gazebo::client::setup(_argc, _argv);
+int main(int _argc, char** _argv) {
+    gazebo::client::setup(_argc, _argv);
 
-	// Create our node for communication
-	gazebo::transport::NodePtr node(new gazebo::transport::Node());
-	node->Init();
+    // Create our node for communication
+    gazebo::transport::NodePtr node(new gazebo::transport::Node());
+    node->Init();
 
-	// Publish to my topic
-	gazebo::transport::PublisherPtr pub =
-		node->Advertise<gazebo::msgs::GzString>("/my_suv_controller/vel_cmd");
+    // Publish to my topic
+    gazebo::transport::PublisherPtr pub =
+        node->Advertise<gazebo::msgs::Int>("/my_suv_controller/vel_cmd");
 
-	// Wait for a subscriber to connect to this publisher
-	pub->WaitForConnection();
+    // Wait for a subscriber to connect to this publisher
+    pub->WaitForConnection();
 
-	char c;
-	while (true) {
-		c = getchar();
-		if (c == 'q') break;
-		char *data = &c;
-		gazebo::msgs::GzString msg;
-		msg.set_data(data);
-		pub->Publish(msg);
-		std::cout << "sent" << std::endl;
-	}
-	//	gazebo::msgs::Twist msg;
-	//	gazebo::msgs::Vector3d linear;
-	//	linear.set_x(1);
-	//	linear.set_y(0);
-	//	linear.set_z(0);
-	//	msg.set_allocated_linear(&linear);
+    int c;
+    std::cout << "Q to quit, w/a/s/d to move, q/e to rotate. "
+                 "type Enter each time."
+              << std::endl;
+    while (true) {
+        c = getchar();
+        if (c == 'Q')
+            break;
+        if (c == '\n')
+            continue;
+        //		char *data = &c;
+        gazebo::msgs::Int msg;
+        msg.set_data(c);
+        pub->Publish(msg);
+        //        std::cout << "sent" << std::endl;
+    }
 
-	gazebo::client::shutdown();
+    gazebo::client::shutdown();
 }
